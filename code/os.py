5254d0a2fa86ad755a8a9aa813f2e2223ae551f6
rockgarden <ro>
2017-10-16 10:44:03 +0800

    add smtp and network text

--- code/os.py
+++ code/os.py
@@ -380,3 +380,130 @@ print ("返回值: %s" % retval)
 
 
 os.chmod(pathFile, stat.S_IRWXU)
+
+
+
+# os.chroot() 方法用于更改当前进程的根目录为指定的目录，使用该函数需要管理员权限。
+# 语法
+# chroot()方法语法格式如下：
+# os.chroot(path);
+# 参数
+# path -- 要设置为根目录的目录。
+# 返回值
+# 该方法没有返回值。
+
+# 设置根目录为 /tmp
+os.chroot("/tmp")
+
+print ("修改根目录成功!!")
+
+
+
+# os.close() 方法用于关闭指定的文件描述符 fd。
+# 语法
+# close()方法语法格式如下：
+# os.close(fd);
+# 参数
+# fd -- 文件描述符。
+# 返回值
+# 该方法没有返回值。
+# 打开文件
+fd = os.open( "foo.txt", os.O_RDWR|os.O_CREAT )
+
+#  写入字符串
+os.write(fd, "This is test")
+
+# 关闭文件
+os.close( fd )
+
+print ("关闭文件成功!!")
+
+
+
+# os.closerange() 方法用于关闭所有文件描述符 fd，从 fd_low (包含) 到 fd_high (不包含), 错误会忽略。
+# 语法
+# closerange()方法语法格式如下：
+# os.closerange(fd_low, fd_high);
+# 参数
+# fd_low -- 最小文件描述符
+# fd_high -- 最大文件描述符
+# 该方法类似于：
+# for fd in xrange(fd_low, fd_high):
+#     try:
+#         os.close(fd)
+#     except OSError:
+#         pass
+# 返回值
+# 该方法没有返回值。
+
+# 打开文件
+fd = os.open("foo.txt", os.O_RDWR|os.O_CREAT )
+
+# 写入字符串
+os.write(fd, "This is test")
+
+# 关闭文件
+os.closerange(fd, fd)
+
+print ("关闭文件成功!!")
+
+
+
+# os.dup() 方法用于复制文件描述符 fd。
+# 语法
+# dup()方法语法格式如下：
+# os.dup(fd);
+# 参数
+# fd -- 文件描述符
+# 返回值
+# 返回复制的文件描述符。
+
+# 打开文件
+fd = os.open("foo.txt", os.O_RDWR|os.O_CREAT )
+
+# 复制文件描述符
+d_fd = os.dup(fd )
+
+# 使用复制的文件描述符写入文件
+os.write(d_fd, "This is test")
+
+# 关闭文件
+os.closerange(fd, d_fd)
+
+print("关闭所有文件成功!!")
+
+
+
+# os.dup2() 方法用于将一个文件描述符 fd 复制到另一个 fd2。
+# Unix, Windows 上可用。
+# 语法
+# dup2()方法语法格式如下：
+# os.dup2(fd, fd2);
+# 参数
+# fd -- 要被复制的文件描述符
+# fd2 -- 复制的文件描述符
+# 返回值
+# 没有返回值。
+
+# 打开文件
+fd = os.open( "foo.txt", os.O_RDWR|os.O_CREAT )
+
+# 写入字符串
+os.write(fd, "This is test")
+
+# 文件描述符为 1000
+fd2 = 1000
+os.dup2(fd, fd2);
+
+# 在新的文件描述符上插入数据
+os.lseek(fd2, 0, 0)
+str = os.read(fd2, 100)
+print ("读取的字符串是 : ", str)
+
+# 关闭文件
+os.close( fd )
+
+print ("关闭文件成功!!")
+
+
+
