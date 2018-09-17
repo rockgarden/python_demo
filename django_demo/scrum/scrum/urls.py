ee4f92a6e0bdc0acc4375e9b3abc35af8fe173bc
Wangkan <rockwk@msn.com>
2018-09-18 00:07:49 +0800

    add
    Signed-off-by: Wangkan <rockwk@msn.com>

---
+++ django_demo/scrum/scrum/urls.py
@@ -0,0 +1,8 @@
+from board.urls import router
+from django.conf.urls import include, url
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    url(r'^api/token/', obtain_auth_token, name='api-token'),
+    url(r'^api/', include(router.urls)),
+]
