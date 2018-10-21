cfaf4c609ba2f037e072a6a916514a2a2a6e6772
Wangkan <rockwk@msn.com>
2018-10-21 21:00:15 +0800

    add
    Signed-off-by: Wangkan <rockwk@msn.com>

--- django_demo/SinglePageWebApp/board/static/board/js/router.js
+++ django_demo/SinglePageWebApp/board/static/board/js/router.js
@@ -1,44 +1,45 @@
 (function ($, Backbone, _, app) {
 
     var AppRouter = Backbone.Router.extend({
-        ///定义路由
         routes: {
-            '': 'home'
+            '': 'home',
+            'sprint/:id': 'sprint' 
         },
         initialize: function (options) {
-            this.contentElement = '#content'; //引用ID选择器，指定Underscore模板加载的位置.
+            this.contentElement = '#content';
             this.current = null;
-            //创建路由时会初始化标题视图，并将其元素添加到<body>的开头。
             this.header = new app.views.HeaderView();
             $('body').prepend(this.header.el);
             this.header.render();
-            Backbone.history.start(); //触发路由
+            Backbone.history.start();
         },
         home: function () {
             var view = new app.views.HomepageView({el: this.contentElement});
             this.render(view);
         },
-
-        //覆盖路由器的默认路由方法。它作为哈希路由传递，并且是回调方法的名称或显式回调函数。
+        sprint: function (id) {
+            var view = new app.views.SprintView({
+                el: this.contentElement,
+                sprintId: id
+            });
+            this.render(view);
+        },
         route: function (route, name, callback) {
             // Override default route to enforce login on every page
             var login;
             callback = callback || this[name];
-            //原始回调函数将被包装，以便在调用之前首先检查身份验证状态。
             callback = _.wrap(callback, function (original) {
                 var args = _.without(arguments, original);
                 if (app.session.authenticated()) {
-                    //如果用户已通过身份验证，则只需调用原始回调。
                     original.apply(this, args);
                 } else {
-                //如果用户未经过身份验证，则隐藏当前页面内容并改为呈现登录视图。当登录视图触发完成事件时，允许继续原始回调。
                     // Show the login screen before calling the view
                     $(this.contentElement).hide();
                     // Bind original callback once the login is successful
                     login = new app.views.LoginView();
                     $(this.contentElement).after(login.el);
                     login.on('done', function () {
-                        this.header.render(); //登录完成后，将再次呈现标题以反映新状态。
+                        this.header.render();
                         $(this.contentElement).show();
                         original.apply(this, args);
                     }, this);
@@ -46,10 +47,9 @@
                     login.render();
                 }
             });
-            //使用新的包装回调调用原始路由。
             return Backbone.Router.prototype.route.apply(this, [route, name, callback]);
         },
-        render: function (view) { //render function 在视图切换时帮助路由追踪
+        render: function (view) {
             if (this.current) {
                 this.current.undelegateEvents();
                 this.current.$el = $();
@@ -60,6 +60,6 @@
         }
     });
     
-    app.router = AppRouter; //路由附加到app配置中让它在整个项目范围内可用.
+    app.router = AppRouter;
 
 })(jQuery, Backbone, _, app);
--- django_demo/backbone/board/static/board/js/views.js
+++ django_demo/backbone/board/static/board/js/views.js
@@ -47,12 +47,16 @@
                 _.map(fieldErrors, appendError, this);
             }, this);
         },
+
+        //serializeForm是表单字段值的更通用的序列化，而不是LoginView中存在的显式版本。
         serializeForm: function (form) {
             return _.object(_.map(form.serializeArray(), function (item) {
                 // Convert object to tuple of (name, value)
                 return [item.name, item.value];
             }));
         },
+
+        //通过阻止默认提交和清除错误来设置表单提交的开始。 扩展此视图的每个视图都需要进一步定义此回调。
         submit: function (event) {
             event.preventDefault();
             this.form = $(event.currentTarget);
@@ -69,6 +73,7 @@
         //{"username": ["This field is required."], "password": ["This field is required."]}
         //{"non_field_errors": ["Unable to login with provided credentials."]}
 
+        //done是触发登录事件的通用版本。它还处理从DOM中删除表单。
         done: function (event) {
             if (event) {
                 event.preventDefault();
@@ -84,36 +89,48 @@
         // 用ID选择器来指派用什么underscore.js模板来渲染主页。
     });
 
+    //LoginView从FormView而不是TemplateView扩展。
     var LoginView = FormView.extend({
         id: 'login',
         templateName: '#login-template',
+
         //此事件属性侦听LoginView元素内任何表单元素上的所有提交事件。触发事件时，它将执行提交回调。
         //提交回调会阻止提交表单，以便视图可以提交并处理结果。
         submit: function (event) {
             var data = {};
+            //提交回调-调用原始的FormView提交，以防止提交并清除任何错误。
+            //调用父类方法(JavaScript无super)
             FormView.prototype.submit.apply(this, arguments);
+            //表单使用serializeForm帮助程序序列化数据，而不是手动检索用户名和密码字段。
             data = this.serializeForm(this.form);
             //通过使用新的app.apiLogin配置，将用户名和密码信息提交给后端。
             $.post(app.apiLogin, data)
                 .done($.proxy(this.loginSuccess, this))
+                //使用默认的failure回调，不必在此重新定义。
                 .fail($.proxy(this.failure, this));
         },
         //登录成功后,将使用先前定义的令牌保存令牌,app.session和登录事件被触发。
+        //loginSuccess回调现在在保存令牌后使用done帮助程序。
         loginSuccess: function (data) {
             app.session.save(data.token);
             this.done();
         }
     });
 
+    //HeaderView从TemplateView扩展，以使逻辑与其他视图保持类似。
     var HeaderView = TemplateView.extend({
+        //与以前的视图不同，tagName已定义。这意味着模板呈现为<header>元素。
         tagName: 'header',
         templateName: '#header-template',
+        //当用户通过身份验证时，标头中的导航将具有两个链接。一个将返回主页，另一个将退出。
         events: {
             'click a.logout': 'logout'
         },
+        //经过身份验证的值将根据当前会话状态传递给模板上下文。如果此状态发生更改，则不会自动更新。必须再次渲染视图。
         getContext: function () {
             return {authenticated: app.session.authenticated()};
         },
+        //注销逻辑-视图中的注销回调中处理。
         logout: function (event) {
             event.preventDefault();
             app.session.delete();
@@ -126,6 +143,7 @@
     //TemplateView是views.js之外不需要的实现细节，因此它仍然是隐藏的。
     app.views.HomepageView = HomepageView;
     app.views.LoginView = LoginView;
+    //视图被添加到app.views映射中，以便路由器可以使用它。
     app.views.HeaderView = HeaderView;
 
 })(jQuery, Backbone, _, app);
