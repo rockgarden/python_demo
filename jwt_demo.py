d09658afde3baf122048e9c9b212d424a09498a5
wangkan <wangakn@eastcom.com>
2022-08-06 16:49:13 +0800

    参数名 algorithms 输入错误

--- jwt_demo.py
+++ jwt_demo.py
@@ -10,7 +10,7 @@ def get_token(id, username, auth):
     token = jwt.encode(
         headers={
             'typ': 'POI',
-            'alg': 'HS256'
+            'alg': 'HS512'
         },
         payload={
             'exp': now + ADMIN_TOKEN_EXPIRE,
@@ -22,7 +22,7 @@ def get_token(id, username, auth):
             }
         },
         key=SECRET_KEY,
-        algorithm='HS256'
+        algorithm='HS512'
     )
     return token
  
@@ -32,17 +32,7 @@ def get_name(token):
     data = jwt.decode(
         jwt=token, 
         key=SECRET_KEY, 
-        algorithm='HS256')
+        algorithms='HS512')
     return data
  
 print('GetName: \n', get_name(get_token(12, 'scorhl', 1)))
-
-	# public static Token create(String username) {
-	# 	HashMap<String, Object> map = new HashMap<String, Object>();
-	# 	map.put("username", username);
-	# 	Date expire_date = new Date(System.currentTimeMillis() + 60L * 1000L * 60L * 2L); //2h
-	# 	String expire_time = ToolsUtil.getDateTimeStr(expire_date, "yyyy-MM-dd HH:mm:ss");
-	# 	String jwt = Jwts.builder().setClaims(map).setExpiration(expire_date).signWith(SignatureAlgorithm.HS512, SECRET)
-	# 			.compact();
-	# 	return new Token("Bearer%20" + jwt, username, expire_time); // jwt前面一般都会加Bearer
-	# }
